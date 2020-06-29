207f553a3782a0e48bd6f8a43f8500479215cc87
Juan Colorado <juancoloradoalvarez@gmail.com>
2020-06-29 18:31:44 -0500

    style: landing store templates
    for this try we are using a template from a collibri web to understand if the style could be the option to the landing for the client.

--- routes/web.php
+++ routes/web.php
@@ -10,3 +10,4 @@ Route::get('/', function () {
 Auth::routes(['verify'=>true]);
 
 Route::get('/home', 'HomeController@index')->name('home')->middleware('verified');
+Route::view('/index', 'store.index');
