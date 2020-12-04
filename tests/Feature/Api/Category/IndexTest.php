08db79675e9c7bbb59c7a8605ccd2ad8df88107c
Juan Colorado <juancoloradoalvarez@gmail.com>
2020-12-04 14:37:12 -0500

    test

---
+++ tests/Feature/Api/Category/IndexTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Feature\Api\Category;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class IndexTest extends TestCase
+{
+ use RefreshDatabase;
+
+    /** @test */
+    public function it_can_test()
+    {
+       //
+    }
+}
